c306f73c7bcc2e317b0f62cc4731293d950fbc20
devops311 <58652764+devops311@users.noreply.github.com>
2020-09-18 13:21:44 +0530

    UI

---
+++ WebApplication5/wwwroot/js/JavaScript.js
@@ -0,0 +1,48 @@
+﻿
+$(document).ready(function () {
+    $('#plus').click(function add() {
+
+        var a = $("#units").val();
+        a++;
+        $("#subs").prop("disabled", !a);
+        $("#units").val(a);
+
+
+
+    });
+    // Set initial disabled state
+    $("#subs").prop("disabled", !$("#units").val());
+
+    $('#subs').click(function subst() {
+
+        var b = $("#units").val();
+        if (b >= 1) {
+            b--;
+            $("#units").val(b);
+            $.ajax({
+                type: "POST",
+                url: "/Home/AddedToCart",
+                data: { 'units': $("#units").val(b) },
+                dataType: 'html',
+
+            });
+        }
+        else {
+            $("#subs").prop("disabled", true);
+        }
+
+    });
+    //$("#btnGet").click(function () {
+    //    $.ajax({
+    //        type: "POST",
+    //        url: "/Home/AddedToCart",
+    //        //data: { 'units': $("#units").val(), 'id': $("#units").val() },
+    //        data: { 'units': $("#units").val(), 'id': $("#units").val(), 'foodname': $("#foodname").val(), 'price': $("#price").val(), 'foodId': $("#foodId").val() },
+    //        dataType: 'html',
+
+    //    });
+    //});
+
+
+});
+
---
+++ WebApplication5/wwwroot/js/JavaScript1.js
@@ -0,0 +1,34 @@
+﻿
+
+$(document).ready(function () {
+    $("#btnclick").click(function () {
+        var i = $("#location").val();
+        var j = $("#restName").val();
+        var k = $("#rating").val();
+        var a = $("#CusineId").val();
+        var b = $("#dist").val();
+        var c = $("#budget").val();
+        if ((i != "" && j != "") && (k != "0.0" && a != "0") && (b != "0" && c != "0")) {
+
+            alert("you can only select one filter at a time");
+
+        }
+        else if ((i != "" || j != "") && (k != "0.0" || a != "0")) {
+            alert("you can only select one filter at a time");
+            return false;
+        }
+        else if ((i != "" || j != "") && (b != "0" || c != "0")) {
+            alert("you can only select one filter at a time");
+            return false;
+        }
+        else if ((k != "0.0" || a != "0") && (b != "0" || c != "0")) {
+            alert("you can only select one filter at a time");
+            return false;
+        }
+        
+        else {
+            return true;
+        }
+
+    });
+});
